a5d793824d97a2f5c1988f5f23cd3e34035dbd45
irahrosete <72952642+irahrosete@users.noreply.github.com>
2024-12-08 22:36:15 +1100

    initial commit :santa:

---
+++ src/main/java/com/aoc/AocApp.java
@@ -0,0 +1,14 @@
+package com.aoc;
+
+public class AocApp {
+    public static void main(String[] args) {
+        System.out.println("""
+            ADVENT OF CODE 2024
+            *     *     *    *
+              *     🦌🛷*     *
+            *     *     *    *
+            🌲   🏠🌲   🌲   🌲
+        🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️
+        """);
+    }
+}
